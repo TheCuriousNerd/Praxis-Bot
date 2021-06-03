88ad5c89fd4d355cebbd989c4c0f570e50431b8f
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2021-06-03 16:53:31 -0400

    Unable to connect to DB

--- standalone_db/standalone_db.py
+++ standalone_db/standalone_db.py
@@ -3,32 +3,16 @@ import asyncio
 from windyquery import DB
 
 db = DB()
+asyncio.new_event_loop().run_until_complete(db.connect('PRAXIS_BOT_DB', {
+    'host' : 'standalone_db',
+    'port' : '5432',
+    'database' : 'PRAXIS_BOT_DB',
+    'username' : 'PRAXIS_BOT',
+    'password' : '#Praxis#Praxis#Praxis',
+}, default=True))
 
-async def init():
-    initConnection()
-    if await isSetup():
-        await setupTables()
-
-
-async def initConnection():
-    asyncio.get_event_loop().run_until_complete(db.connect('db_name', {
-        'host' : 'localhost',
-        'port' : '5432',
-        'database' : 'PRAXIS_BOT_DB',
-        'username' : 'PRAXIS_BOT',
-        'password' : '#Praxis#Praxis#Praxis',
-    }, default=True))
-
-async def isSetup():
-    return False
-
-async def setupTables():
-    await db.schema('TABLE IF NOT EXISTS Praxis_KeyValues_String').create(
-        'id             serial PRIMARY KEY',
-        'key            text',
-        'value          text'
-    )
-
+def init():
+    print("test")
 
 if __name__ == "__main__":
     init()
