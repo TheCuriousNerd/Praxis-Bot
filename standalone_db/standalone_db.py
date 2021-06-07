e0ec5175dc9ee4b1792c309cdbaa419f8254ad70
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2021-06-07 15:44:14 -0400

    Async doesnt like to play nice

--- standalone_db/standalone_db.py
+++ standalone_db/standalone_db.py
@@ -18,29 +18,39 @@ def init():
     'username' : 'PRAXIS_BOT',
     'password' : 'PraxisPraxisPraxis',
 }, default=True))
+    print("db: " , db)
 
 
 def maintest(db:DB):
-    db.schema('TABLE users').create(
-    'id            serial PRIMARY KEY',
-    'group_id      integer references groups (id) ON DELETE CASCADE',
-    'created_at    timestamp not null DEFAULT NOW()',
-    'email         text not null unique',
-    'name         text not null',
-    'is_admin      boolean not null default false',
-    'address       jsonb',
-    'payday        integer not null',
-    'CONSTRAINT unique_email UNIQUE(group_id, email)',
-    'check(payday > 0 and payday < 8)'
-    )
+    async def make_table():
+        db.schema('TABLE users').create(
+            'id            serial PRIMARY KEY',
+            'group_id      integer references groups (id) ON DELETE CASCADE',
+            'created_at    timestamp not null DEFAULT NOW()',
+            'email         text not null unique',
+            'name         text not null',
+            'is_admin      boolean not null default false',
+            'address       jsonb',
+            'payday        integer not null',
+            'CONSTRAINT unique_email UNIQUE(group_id, email)',
+            'check(payday > 0 and payday < 8)'
+            )
 
-    db.table('users').insert(
-    {'id': 1, 'name': 'Tom'},
-    {'id': 2, 'name': 'Jerry'},
-    {'id': 3, 'name': 'DEFAULT'}
-    )
+    async def add_users():
+        db.table('users').insert(
+            {'id': 1, 'name': 'Tom'},
+            {'id': 2, 'name': 'Jerry'},
+            {'id': 3, 'name': 'DEFAULT'}
+            )
+    async def get_user():
+        result = db.table('users').select('id', 'name')
+        return result
 
-    users = db.table('users').select('id', 'name')
+    asyncio.get_event_loop().run_until_complete(make_table())
+    asyncio.get_event_loop().run_until_complete(add_users())
+
+    users = asyncio.get_event_loop().run_until_complete(get_user())
+
 
     print(type(users))
     print(users)
@@ -48,21 +58,36 @@ def maintest(db:DB):
     print(users[0]['name'])
 
 
-async def Async_maintest_worker(db:DB):
-    await db.schema('TABLE users').create(
-    'id            serial PRIMARY KEY',
-    'group_id      integer references groups (id) ON DELETE CASCADE',
-    'created_at    timestamp not null DEFAULT NOW()',
-    'name          text not null'
-    )
+def Async_maintest(db:DB):
+    async def make_table():
+        await db.schema('TABLE users').create(
+            'id            serial PRIMARY KEY',
+            'group_id      integer references groups (id) ON DELETE CASCADE',
+            'created_at    timestamp not null DEFAULT NOW()',
+            'email         text not null unique',
+            'name         text not null',
+            'is_admin      boolean not null default false',
+            'address       jsonb',
+            'payday        integer not null',
+            'CONSTRAINT unique_email UNIQUE(group_id, email)',
+            'check(payday > 0 and payday < 8)'
+            )
 
-    await db.table('users').insert(
-    {'id': 1, 'name': 'Tom'},
-    {'id': 2, 'name': 'Jerry'},
-    {'id': 3, 'name': 'DEFAULT'}
-    )
+    async def add_users():
+        await db.table('users').insert(
+            {'id': 1, 'name': 'Tom'},
+            {'id': 2, 'name': 'Jerry'},
+            {'id': 3, 'name': 'DEFAULT'}
+            )
+    async def get_user():
+        result = await db.table('users').select('id', 'name')
+        return result
 
-    users = await db.table('users').select('id', 'name')
+    asyncio.get_event_loop().run_until_complete(make_table())
+    asyncio.get_event_loop().run_until_complete(add_users())
+
+    users = asyncio.get_event_loop().run_until_complete(get_user())
+
 
     print(type(users))
     print(users)
@@ -71,5 +96,5 @@ async def Async_maintest_worker(db:DB):
 
 if __name__ == "__main__":
     init()
-    asyncio.run(Async_maintest_worker(db))
-    #maintest(db)
+    #Async_maintest(db)
+    maintest(db)
