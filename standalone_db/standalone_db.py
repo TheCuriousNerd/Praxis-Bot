1142c3c86fda8833d4e73c540f6029354d4c73af
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2021-06-09 16:41:40 -0400

    Technically Working

--- standalone_db/standalone_db.py
+++ standalone_db/standalone_db.py
@@ -29,6 +29,12 @@ import time
 import flask
 import sqlalchemy as db
 
+import os
+import bot_functions.praxis_logging as praxis_logging
+praxis_logger_obj = praxis_logging.praxis_logger()
+praxis_logger_obj.init(os.path.basename(__file__))
+praxis_logger_obj.log("\n -Starting Logs: " + os.path.basename(__file__))
+
 user = "PRAXIS_BOT"
 password = "PraxisPraxisPraxis"
 hostName = "standalone_db_main"
@@ -50,7 +56,7 @@ def test_init():
             'DROP TABLE users'
         )
     except:
-        print("Couldn't Drop it")
+        praxis_logger_obj.log("Couldn't Drop it")
 
     try:
         dbConnection.execute(
@@ -59,7 +65,7 @@ def test_init():
         'name TEXT);'
     )
     except:
-        print("Couldn't Make it")
+        praxis_logger_obj.log("Couldn't Make it")
 
     try:
         dbConnection.execute(
@@ -86,18 +92,69 @@ def test_init():
         )
 
         for item in results:
-            print(item)
+            praxis_logger_obj.log(item)
     except:
         pass
 
 
-@api.route('/api/v1/', methods=['GET'])
-def get_data():
-    return flask.make_response('BOO', 200)
+def init():
+    global dbConnection
+    dbConnection = db.create_engine(connectionString)
+    try:
+        dbConnection.execute(
+            'DROP TABLE basic_key_vars'
+        )
+        praxis_logger_obj.log("[Dropped]: (basic_key_vars)")
+    except:
+        praxis_logger_obj.log("[Couldn't Drop it]: (basic_key_vars)")
+    try:
+        dbConnection.execute(
+        'CREATE TABLE basic_key_vars ('
+        'id SERIAL, '
+        'key TEXT, '
+        'var TEXT);'
+        )
+        praxis_logger_obj.log("[Table Created]: (basic_key_vars)")
+    except:
+        praxis_logger_obj.log("[Table Creation Failed or Already Exists]: (basic_key_vars)")
 
-@api.route('/api/v1/', methods=['GET'])
-def set_data():
-    return flask.make_response('BOO', 200)
+def does_basic_key_exist(key):
+    global dbConnection
+    try:
+        returns = ""
+        results = dbConnection.execute(
+        'SELECT * FROM '
+        'basic_key_vars '
+        'WHERE key = \'%s\';' % (key)
+        )
+        for item in results:
+            returns = returns + str(item) + " "
+            praxis_logger_obj.log(item)
+        return returns
+    except:
+        return False
+
+def set_basic_data(key, var):
+    global dbConnection
+    try:
+        dbConnection.execute(
+        'INSERT INTO basic_key_vars '
+        '(key, var) '
+        'VALUES (\'%s\',\'%s\');' % (key, var)
+        )
+        return 'Insertion Done'
+    except:
+        return 'Insertion Failed'
+
+@api.route('/api/v1/get_data/<key_name>', methods=['GET'])
+def get_data(key_name):
+    result = does_basic_key_exist(key_name)
+    return flask.make_response(str(result), 200)
+
+@api.route('/api/v1/set_data/<key_name>/<var_string>', methods=['GET'])
+def set_data(key_name, var_string):
+    result = set_basic_data(key_name, var_string)
+    return flask.make_response(str(result), 200)
 
 @api.route('/', methods=['GET'])
 def command_check():
@@ -105,5 +162,5 @@ def command_check():
 
 if __name__ == "__main__":
     time.sleep(5)
-    test_init()
+    init()
     api.run(host='0.0.0.0', port=42002)
