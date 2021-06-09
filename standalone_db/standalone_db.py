9be388341b056521ccef5d63b413652d453d2d0e
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2021-06-09 15:05:20 -0400

    Added Flask

--- standalone_db/standalone_db.py
+++ standalone_db/standalone_db.py
@@ -1,3 +1,27 @@
+# The main repository of Praxis Bot can be found at: <https://github.com/TheCuriousNerd/Praxis-Bot>.
+# Copyright (C) 2021
+
+# Author Info Examples:
+# Name / Email / Website
+# Twitter / Twitch / Youtube
+
+# Authors:
+#   Alex Orid / inquiries@thecuriousnerd.com / TheCuriousNerd.com
+#       Twitter: @TheCuriousNerd / Twitch: TheCuriousNerd / Youtube: thecuriousnerd / Github: TheCuriousNerd
+
+# This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import asyncio
 
 import time
@@ -12,6 +36,10 @@ databaseName = "PRAXIS_BOT_DB"
 connectionString ="postgresql://%s:%s@%s/%s" % (user, password, hostName, databaseName)
 
 dbConnection = None
+
+api = flask.Flask(__name__)
+# enable/disable this to get web pages of crashes returned
+api.config["DEBUG"] = False
 
 def test_init():
     global dbConnection
@@ -63,6 +91,19 @@ def test_init():
         pass
 
 
+@api.route('/api/v1/', methods=['GET'])
+def get_data():
+    return flask.make_response('BOO', 200)
+
+@api.route('/api/v1/', methods=['GET'])
+def set_data():
+    return flask.make_response('BOO', 200)
+
+@api.route('/', methods=['GET'])
+def command_check():
+    return flask.make_response('BOO', 200)
+
 if __name__ == "__main__":
     time.sleep(5)
     test_init()
+    api.run(host='0.0.0.0', port=42002)
