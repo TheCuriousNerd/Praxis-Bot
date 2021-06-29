f19d27b0e66c732522d03757cb7787e7825f933c
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2021-06-29 17:50:28 -0400

    Update temptest_2.py

--- temptest_2.py
+++ temptest_2.py
@@ -2,23 +2,43 @@ import pyparsing
 
 def init():
     stringToTest = "This is a $(test) command @(user)"
+    #stringToTest = "$(test$(test$(test)))$(test)$(test)"
     print(searchForFunctions1(stringToTest))
-    #print(searchForFunctions2(stringToTest))
+
+    print(stringFunctionParser(stringToTest))
+
+def stringFunctionParser(
+        input:str = "",
+        arguments:list = []
+        ):
+    curStr = input
+
+    def replace_Arguments(input:str):
+        return input
+    def replace_Variables(input:str):
+        return input
+    def run_Functions(input:str):
+        return input
+
+    curStr = replace_Arguments(curStr)
+    curStr = replace_Variables(curStr)
+    curStr = run_Functions(curStr)
+
+    return curStr
+
+
+def test_function(input):
+    result = "TestResult_" + input + input
+    return result
+
+def test_function2(input):
+    result = "TestResult $(TF) " + input + " " + input
+    return result
 
 def searchForFunctions1(input):
-        parserContent = pyparsing.Word(pyparsing.alphanums)
+        input = "$(%s)" % input
+        #parserContent = pyparsing.Word(pyparsing.alphanums)
         parser = pyparsing.nestedExpr('$(', ')')
-        testResult_ = parser.parseString(input)
-        testResult = testResult_.asList()
-
-        #result1 = ("functionName", input, False)
-        #result2 = ("functionName2", ("functionName3", input, False, None), True)
-        #results = []
-        return testResult
-
-def searchForFunctions2(input):
-        parserContent = pyparsing.Word(pyparsing.alphanums)
-        parser = pyparsing.nestedExpr('@(', ')')
         testResult_ = parser.parseString(input)
         testResult = testResult_.asList()
 
