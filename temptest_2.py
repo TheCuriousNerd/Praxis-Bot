e65842ff60653b457fcbdafca9587c13246eb3a3
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2021-07-27 15:55:11 -0400

    Update temptest_2.py

--- temptest_2.py
+++ temptest_2.py
@@ -2,6 +2,7 @@ from enum import Enum, auto
 import pyparsing
 
 class TokenType(Enum):
+    NONE = auto()
     ARGUMENT = auto()
     FUNCTION = auto()
     VARIABLE = auto()
@@ -22,95 +23,81 @@ def stringFunctionParser(
         ):
     curStr = input
 
-    def replace_Arguments(input:str):
-        return processString(input, TokenType.ARGUMENT)
-    def replace_Variables(input:str):
-        return processString(input, TokenType.VARIABLE)
-    def run_Functions(input:str):
-        return processString(input, TokenType.FUNCTION)
-
-    curStr = replace_Arguments(curStr)
-    #print(curStr)
-    curStr = replace_Variables(curStr)
-    #print(curStr)
-    curStr = run_Functions(curStr)
-    #print(curStr)
+    curStr = processString(input, arguments)
 
     return curStr
 
-def stringCleanup(input):
+def stringCleanup(
+        input:str = "",
+        arguments:list = []
+        ):
     results = ""
     for i in input[0]:
         results = results + i + " "
     return input
 
-def processString(input, tokenType):
-    results = new_Search(input, tokenType)
+def processString(
+        input:str = "",
+        arguments:list = []
+        ):
+    results = searchPrep(input)
     print(results)
-    for result in results[0]:
-        if type(result) == list:
-            print("LIST FOUND:")
-            print(result)
-        else:
-            print(result)
 
-    def processor(p_Input):
-        if tokenType == TokenType.ARGUMENT:
-            pass
-        elif tokenType == TokenType.FUNCTION:
-            pass
-        elif tokenType == TokenType.VARIABLE:
-            pass
+    print("cur test:")
+    for results in results[0]:
+        if type(results) == list:
+            print("{Token FOUND}:")
+            print(results)
+            print(len(results))
+
+            if len(results) > 1:
+                for r in results:
+                    selectedToken = TokenType.NONE
+                    if "#" in r[0]:
+                        selectedToken = TokenType.ARGUMENT
+                        print(processToken(r[0], arguments, selectedToken))
+                    elif "@" in r[0]:
+                        selectedToken = TokenType.VARIABLE
+                        print(processToken(r[0], arguments, selectedToken))
+                    else:
+                        selectedToken = TokenType.FUNCTION
+                        print(processToken(r, arguments, selectedToken))
+            else:
+                selectedToken = TokenType.NONE
+                if "#" in results[0]:
+                    selectedToken = TokenType.ARGUMENT
+                    print(processToken(results[0], arguments, selectedToken))
+                elif "@" in results[0]:
+                    selectedToken = TokenType.VARIABLE
+                    print(processToken(results[0], arguments, selectedToken))
+                else:
+                    selectedToken = TokenType.FUNCTION
+                    print(processToken(results[0], arguments, selectedToken))
+
 
     return input
 
-def new_Search(input, tokenType):
+def processToken(input, arguments, targetToken):
+    returnString = ""
+    print(input)
+    if targetToken == TokenType.ARGUMENT:
+        print("{FOUND a ARGUMENT}")
+    elif targetToken == TokenType.VARIABLE:
+        print("{FOUND a VARIABLE}")
+    elif targetToken == TokenType.FUNCTION:
+        print("{FOUND a FUNCTION}")
+    return returnString
+
+def searchPrep(input):
     parserContent = pyparsing.Word(pyparsing.alphanums) | ' '
 
-    if tokenType == TokenType.ARGUMENT:
-        input = "$(%s)" % input
-        parser = pyparsing.nestedExpr('$(', ')')
-        results = parser.parseString(input)
-        formattedResults = results.asList()
-        print("Arguments Search:")
-        return formattedResults
-    elif tokenType == TokenType.FUNCTION:
-        input = "$(%s)" % input
-        parser = pyparsing.nestedExpr('$(', ')')
-        results = parser.parseString(input)
-        formattedResults = results.asList()
-        print("Functions Search:")
-        return formattedResults
-    elif tokenType == TokenType.VARIABLE:
-        input = "$(%s)" % input
-        parser = pyparsing.nestedExpr('$(', ')')
-        results = parser.parseString(input)
-        formattedResults = results.asList()
-        print("Variables Search:")
-        return formattedResults
+    input = "$(%s)" % input
+    parser = pyparsing.nestedExpr('$(', ')')
+    results = parser.parseString(input)
+    formattedResults = results.asList()
 
+    return formattedResults
 
-def test_function(input):
-    result = "TestResult_" + input + input
-    return result
-
-def test_function2(input):
-    result = "TestResult $(TF) " + input + " " + input
-    return result
-
-def searchForFunctions1(input):
-        #print(input)
-        input = "$(%s)" % input
-        parserContent = pyparsing.Word(pyparsing.alphanums) | ' '
-        parser = pyparsing.nestedExpr('$(', ')')
-        testResult_ = parser.parseString(input)
-        #print(testResult_)
-        testResult = testResult_.asList()
-
-        #result1 = ("functionName", input, False)
-        #result2 = ("functionName2", ("functionName3", input, False, None), True)
-        #results = []
-        return testResult
 
 if __name__ == '__main__':
     init()
