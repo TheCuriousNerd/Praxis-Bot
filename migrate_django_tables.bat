7e6dac9b147d2d2aec2d5a9b539d1402ab18db9f
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2022-03-14 17:48:11 -0400

    table reset script

--- migrate_django_tables.bat
+++ migrate_django_tables.bat
@@ -1,2 +1,3 @@
+echo "WARNING: This will not work if the settings are not configured correctly."
 cd "standalone-core\user_client\v2"
-.\env\Scripts\activate.bat & python manage.py makemigrations & python manage.py migrate --database=localhost & deactivate & exit
+.\env\Scripts\activate.bat & python manage.py makemigrations & python manage.py migrate & deactivate & exit
---
+++ settings_for_setup_postsetup.bat
@@ -0,0 +1,4 @@
+cd "standalone-core\user_client\v2\core"
+ren "settings.py" "settings_local.py"
+ren "settings.py.bak" "settings.py"
+exit
