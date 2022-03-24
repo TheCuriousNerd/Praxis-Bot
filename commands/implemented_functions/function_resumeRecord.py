2a48f0fa82963cd8e6f87ab84dbb1ff98af33039
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2022-03-23 21:40:38 -0400

    Added obs functions
    Added:
    
    $isLive
    $isRecording
    $startRecording
    $stopRecording
    $pauseRecording
    $resumeRecording
    $startStream
    $stopStream

---
+++ commands/implemented_functions/function_resumeRecord.py
@@ -0,0 +1,79 @@
+# The main repository of Praxis Bot can be found at: <https://github.com/TheCuriousNerd/Praxis-Bot>.
+# Copyright (C) 2021
+
+# Author Info Examples:
+#   Name / Email / Website
+#       Twitter / Twitch / Youtube / Github
+
+# Authors:
+#   Alex Orid / inquiries@thecuriousnerd.com / TheCuriousNerd.com
+#       Twitter: @TheCuriousNerd / Twitch: TheCuriousNerd / Youtube: thecuriousnerd / Github: TheCuriousNerd
+
+# This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from abc import ABCMeta
+from ast import arg
+
+from json import loads
+from urllib.parse import urlencode
+import requests
+
+from commands.command_base import AbstractCommand
+from commands.command_functions import AbstractCommandFunction
+
+from commands.command_functions import Abstract_Function_Helpers
+
+from bot_functions import utilities_script as utility
+
+import simpleobsws
+import config
+
+class Function_resumeRecording(AbstractCommandFunction, metaclass=ABCMeta):
+    """
+    This is v0 of Functions
+    """
+    functionName = "resumeRecording"
+    helpText = ["This is a v0 function.",
+        "\nExample:","testFunction"]
+
+    def __init__(self):
+        super().__init__(
+            functionName = Function_resumeRecording.functionName,
+            n_args = 0,
+            functionType = AbstractCommandFunction.FunctionType.ver0,
+            helpText = Function_resumeRecording.helpText,
+            bonusFunctionData = None
+            )
+
+    def do_function(self, tokenSource, user, functionName, args, bonusData):
+        output = self.do_work(user, functionName, args, bonusData)
+
+        return output
+
+    def do_work(self, user, functionName, args, bonusData):
+
+        try:
+            from bot_functions import obsWebSocket as obsWebSocket
+            output:simpleobsws.RequestResponse = obsWebSocket.makeRequest("ResumeRecord", {})
+            print(output)
+            if output.responseData.get("waitForResult", None) == None:
+                return "[Is OBS Running?]"
+            else:
+                return str(output.responseData.get("waitForResult"))
+
+        except Exception as e:
+            # todo handle exceptions
+            return str(e)
+
+        return ""
