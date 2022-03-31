8232ec293f84a96fd6a2cc25cecd775b1a3852c2
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2022-03-31 07:17:11 -0400

    doc stuff

--- function_doc_generator.py
+++ function_doc_generator.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     for key_ in loadedFunctions.keys():
         functionList.append(key_)
     functionList.sort()
+
+    print("\033c") #Clear the screen first
+    print("\n# Function List")
     for f in functionList:
-        print("\nName: ")
-        print("$" + type(loadedFunctions[f]).functionName)
-        print("Description: ")
+        print("\n## Function Name: $"+ type(loadedFunctions[f]).functionName)
+        print("### Description: ")
         for line_ in type(loadedFunctions[f]).helpText:
             print(line_)
